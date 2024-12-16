1630407a70e5e4fe67ee3efc3998f2548660f943
agd03 <agracianid@correo.ugr.es>
2024-12-16 13:39:45 +0100

    Añadido proyecto Android

---
+++ ANDROID/NPI_app/app/src/main/java/com/example/npi_app/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.npi_app
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.npi_app.ui.theme.NPI_appTheme
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Aquí puedes manipular el TextView
+        val tituloMenu = findViewById<TextView>(R.id.titulo_menu)
+        tituloMenu.text = "MENÚ PRINCIPAL"
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NPI_appTheme {
+        Greeting("Android")
+    }
+}
