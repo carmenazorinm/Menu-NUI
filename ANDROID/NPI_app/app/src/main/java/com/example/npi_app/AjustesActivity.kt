6a768a1f73c752ee6fa0dff0d6a8026fc182f597
agd03 <agracianid@correo.ugr.es>
2024-12-22 18:58:49 +0100

    Traducción, falta algún retoque

--- ANDROID/NPI_app/app/src/main/java/com/example/npi_app/AjustesActivity.kt
+++ ANDROID/NPI_app/app/src/main/java/com/example/npi_app/AjustesActivity.kt
@@ -1,12 +1,41 @@
 package com.example.npi_app
 
 import android.os.Bundle
+import android.widget.ImageButton
+import java.util.Locale
 
 class AjustesActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ajustes)
 
-        // Configura aquí la lógica de tu pantalla de ajustes si es necesario
+        // Referencia a los botones de idioma
+        val btnEspanol = findViewById<ImageButton>(R.id.btn_espanol)
+        val btnIngles = findViewById<ImageButton>(R.id.btn_ingles)
+        val btnAleman = findViewById<ImageButton>(R.id.btn_aleman)
+
+        // Configuración de los listeners para cambiar el idioma
+        btnEspanol.setOnClickListener {
+            setLocale("es") // Cambia a español
+        }
+
+        btnIngles.setOnClickListener {
+            setLocale("en") // Cambia a inglés
+        }
+
+        btnAleman.setOnClickListener {
+            setLocale("de") // Cambia a alemán
+        }
+    }
+
+    private fun setLocale(language: String) {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+        val config = resources.configuration
+        config.setLocale(locale)
+        resources.updateConfiguration(config, resources.displayMetrics)
+
+        // Reinicia la actividad para aplicar los cambios de idioma
+        recreate()
     }
 }
